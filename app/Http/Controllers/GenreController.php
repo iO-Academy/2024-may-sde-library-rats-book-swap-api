b7891be0b7f8028bf079803f69f3e3287cc94b49
JordanVanBuul <drizz1995@hotmail.com>
2024-08-12 15:42:12 +0100

    ready for review

--- app/Http/Controllers/GenreController.php
+++ app/Http/Controllers/GenreController.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Genre;
 use Illuminate\Http\Request;
 
 class GenreController extends Controller
 {
-    //
+    public Genre $genre;
+
+    public function __construct(Genre $genre)
+    {
+        $this->genre = $genre;
+    }
+
+    public function getAllGenres()
+    {
+        $genres = $this->genre->get();
+        return response()->json([
+            'message' => 'Genres retrieved',
+            'success' => true,
+            'data' => $genres
+        ]);
+    }
 }
