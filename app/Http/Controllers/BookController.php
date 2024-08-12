e362cbb2f59fb486e577b043f9ec22c0181c4b54
Andy Bool <andybool50@gmail.com>
2024-08-12 12:26:13 +0100

    getAllBooks method completed

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
 use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
-    //
+    public function getAllBooks()
+    {
+    $books = Book::all()->makeHidden(['blurb', 'claimed_by_name', 'page_count', 'claimed', 'user_id', 'created_at', 'updated_at' ]);
+
+    return response()->json([
+        'message' => 'Books successfully retrieved',
+        'success'=> true,
+        'data' => $books
+    ]);
+    }
 }
